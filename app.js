0873f755100fb8e289968441668006e251773856
julianacpeng-boop <julianacp.eng@gmail.com>
2026-06-13 11:11:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-function testarEnvio() {
-  fetch("https://script.google.com/macros/s/AKfycby9NyvrwB6FTjGN_n6Y9z8Ig56mdaG0HKgvDdyMNDpmZIZWu93rm-5T-kx30hJq_VYVZA/exec", {
-    method: "POST",
-    body: JSON.stringify({
-      chamado: "TESTE001",
-      tecnico: "João Teste",
-      dataConclusao: "13/06/2026",
-      horaConclusao: "10:00",
-      nota: 5,
-      comentario: "Teste do sistema"
-    })
-  })
-  .then(r => r.text())
-  .then(res => {
-    alert("Resposta: " + res);
-  })
-  .catch(err => {
-    alert("Erro: " + err);
-  });
+function salvarPendente(dados) {
+  let pendencias = JSON.parse(localStorage.getItem("pendencias")) || [];
+
+  pendencias.push(dados);
+
+  localStorage.setItem("pendencias", JSON.stringify(pendencias));
+
+  console.log("Salvo offline:", dados);
 }
