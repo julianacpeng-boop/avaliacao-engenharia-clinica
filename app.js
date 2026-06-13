fe640c9416437552d7ee5472f672232ee7b3595e
julianacpeng-boop <julianacp.eng@gmail.com>
2026-06-13 10:56:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-fetch("https://script.google.com/macros/s/AKfycby9NyvrwB6FTjGN_n6Y9z8Ig56mdaG0HKgvDdyMNDpmZIZWu93rm-5T-kx30hJq_VYVZA/exec", {
-  method: "POST",
-  body: JSON.stringify({
-    chamado: "123",
-    tecnico: "João",
-    dataConclusao: "13/06/2026",
-    horaConclusao: "10:30",
-    nota: 5,
-    comentario: "OK"
+function testarEnvio() {
+  fetch("https://script.google.com/macros/s/AKfycby9NyvrwB6FTjGN_n6Y9z8Ig56mdaG0HKgvDdyMNDpmZIZWu93rm-5T-kx30hJq_VYVZA/exec", {
+    method: "POST",
+    body: JSON.stringify({
+      chamado: "TESTE001",
+      tecnico: "João Teste",
+      dataConclusao: "13/06/2026",
+      horaConclusao: "10:00",
+      nota: 5,
+      comentario: "Teste do sistema"
+    })
   })
-})
+  .then(r => r.text())
+  .then(res => {
+    alert("Resposta: " + res);
+  })
+  .catch(err => {
+    alert("Erro: " + err);
+  });
+}
