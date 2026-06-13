62f8a0a01f161bf1c8571cc0d11f7b02c5ddfcc9
julianacpeng-boop <julianacp.eng@gmail.com>
2026-06-13 14:04:26 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,12 @@
-const CACHE_NAME = "avaliacao-ec-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json"
-];
-
-// INSTALAÇÃO
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-// INTERCEPTA REQUISIÇÕES
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-});
-
-// ATUALIZA CACHE
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("app-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/app.js"
+      ]);
     })
   );
 });
